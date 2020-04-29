7c2e14b9bf4c177cd8a2394cd0141f4af912939d
Jonathan Lui <jonslui@gmail.com>
2020-04-29 12:32:58 -0400

    vertical/horizontal win added, cases for get_choice() added

--- connect_four.rb
+++ connect_four.rb
@@ -10,7 +10,6 @@ class Player
 end
 
 
-
 class ConnectFour
     attr_accessor :white, :black, :board
 
@@ -27,16 +26,27 @@ class ConnectFour
             self.white
             choice = get_choice(self.white)
             add_choice_to_board(choice, self.white)
+
+            if check_for_win(self.white) == true
+                print_display
+                return
+            end
+
             self.white.turn = false
         else
             self.black
             choice = get_choice(self.black)
             add_choice_to_board(choice, self.black)
+            if check_for_win(self.black) == true
+                print_display
+                return
+            end
             self.white.turn = true
         end
         print_display
         gameloop
     end
+
 
     def print_display
         counter = 5
@@ -54,13 +64,25 @@ class ConnectFour
         puts " 1  2  3  4  5  6  7 "
     end
 
+
     def get_choice(player)
         choice = ""
         until choice.match?(/[1-7]/) == true
             print "#{player.name}, choose a column: "
-            choice = gets
-        end
+            choice = gets.chomp
+            
+            # make sure number chosen is not more than 1 didgit long
+            if choice.length != 1
+                puts "Column must be between 1 and 7: "
+                choice = ""
+            
+            # if there are already 6 peices in that column, prompt again
+            elsif self.board[choice.to_i - 1].length == 6
+                puts "That column is full!"
+                choice = ""
+            end
 
+        end
         return choice
     end
 
@@ -73,7 +95,56 @@ class ConnectFour
             puts
         else
             # add case for if column height already exceeds 6
+
         end
+    end
+
+
+    def check_for_win(player)
+
+        # check for vertical win
+        player.choices.each do |column|
+            tmp = column
+            while tmp.length > 3 
+                if tmp[1] == tmp[0] + 1 && tmp[2] == tmp[1] +1 && tmp[3] == tmp[2] + 1
+                    puts "#{player.name} wins!"
+                    return true
+                end
+                tmp = tmp.slice(1..-1)
+            end
+        end
+
+        # check for horizontal win
+        6.times do 
+            counter = 0
+            row = []
+            
+            # add all values in the first row of each column to row
+            row << player.choices[0][counter] 
+            row << player.choices[1][counter] 
+            row << player.choices[2][counter] 
+            row << player.choices[3][counter] 
+            row << player.choices[4][counter] 
+            row << player.choices[5][counter] 
+            row << player.choices[6][counter] 
+
+            while row.length > 3
+                if row[0] == counter && row[1] == counter && row[2] == counter && row[3] == counter
+                    puts "#{player.name} wins!"
+                    return true
+                end
+            row = row.slice(1 ..  -1)
+            end
+
+            counter +=1
+        end
+
+        # check for diagonal win
+        # 24 cases, 12 mirrored
+
+        
+
+        return false
     end
 end
 
